982a6682499557c3ecedfb6ac2ad274e1a4aca2c
Anayatc <anayatc@gmail.com>
2017-05-09 11:38:51 +0100

    more django setup

--- CyberPhish/settings.py
+++ CyberPhish/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'URL_Expander.apps.UrlExpanderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- URL_Expander/models.py
+++ URL_Expander/models.py
@@ -2,4 +2,5 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class URL(models.Model):
+    url = models.CharField(max_length=1000)
