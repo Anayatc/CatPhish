113026bbda0fad087d7911797c045d5dac3107dd
Anayatc <anayatc@gmail.com>
2017-05-09 10:26:46 +0100

    filled in url expand function

--- URL_Expander/URL_Expand.py
+++ URL_Expander/URL_Expand.py
@@ -6,7 +6,6 @@ def url_resolve(url):
     resp = session.head(url, allow_redirects=True)
     return resp.url
 
-
 print(url_resolve('http://amzn.to/2pfDiJE'))
 print(url_resolve('https://goo.gl/Oha1fI'))
 print(url_resolve('http://ow.ly/nFlK30byASt'))
