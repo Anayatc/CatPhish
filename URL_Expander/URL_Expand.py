b1427ec261521f0c0fad42e388bf22fcdc752961
Anayatc <anayatc@gmail.com>
2017-05-09 11:07:36 +0100

    moved templates directory, and tidied things up a bit

--- URL_Expander/URL_Expand.py
+++ URL_Expander/URL_Expand.py
@@ -5,6 +5,8 @@ def url_resolve(url):
     session = requests.Session()
     resp = session.head(url, allow_redirects=True)
     return resp.url
+# takes url as input and returns full url if url has been shortened.
+
 
 print(url_resolve('http://amzn.to/2pfDiJE'))
 print(url_resolve('https://goo.gl/Oha1fI'))
--- URL_Expander/views.py
+++ URL_Expander/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.template import Context, loader
 
 
 def index(request):
-    return HttpResponse('index.html')
+    template = loader.get_template('CyberPhish/URL_Expander/templates/index.html')
+    return HttpResponse(template.render())
