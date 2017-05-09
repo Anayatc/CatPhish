b0f6e22e3c5e9404e664a0336d04f1dc03f6b09f
Anayatc <anayatc@gmail.com>
2017-05-09 09:44:46 +0100

    refactored function

--- URL_Expander/URL_Expand.py
+++ URL_Expander/URL_Expand.py
@@ -1,18 +1,4 @@
-import http.client
-import urllib.parse
-import urllib.request
 import requests
-
-
-def url_expand(url):
-    parsed = urllib.parse.urlparse(url)
-    h = http.client.HTTPConnection(parsed.netloc)
-    h.request('HEAD', parsed.path)
-    response = h.getresponse()
-    if response.status/100 == 3 and response.getheader('Location'):
-        return response.getheader('Location')
-    else:
-        return url + ' is direct path'
 
 
 def url_resolve(url):
