486fae1ff836a7058932ce4eb6cdb33535774dc7
Anayatc <anayatc@gmail.com>
2017-05-09 09:12:39 +0100

    filled in url expand function

--- URL_Expander/URL_Expand.py
+++ URL_Expander/URL_Expand.py
@@ -1,2 +1,11 @@
-def url_expand():
-    pass
+import requests
+
+
+def url_expand(url):
+    response = requests.get(url)
+    if response.history:
+        for resp in response.history:
+            return response.status_code, resp.url
+        return response.status_code, response.url
+    else:
+        return 'Error with url'
