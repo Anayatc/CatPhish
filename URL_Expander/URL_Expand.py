f12fc9282cdf7c6c22501764e8160938f6793ffd
Anayatc <anayatc@gmail.com>
2017-05-09 09:28:45 +0100

    filled in url expand function

--- URL_Expander/URL_Expand.py
+++ URL_Expander/URL_Expand.py
@@ -1,11 +1,17 @@
-import requests
+import http.client
+import urllib.parse
 
 
 def url_expand(url):
-    response = requests.get(url)
-    if response.history:
-        for resp in response.history:
-            return response.status_code, resp.url
-        return response.status_code, response.url
+    parsed = urllib.parse.urlparse(url)
+    h = http.client.HTTPConnection(parsed.netloc)
+    h.request('HEAD', parsed.path)
+    response = h.getresponse()
+    if response.status/100 == 3 and response.getheader('Location'):
+        return response.getheader('Location')
     else:
-        return 'Error with url'
+        return url + ' is direct path'
+
+
+print(url_expand('http://amzn.to/2pfDiJE'))
+print(url_expand('https://goo.gl/Oha1fI'))
