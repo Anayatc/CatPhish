8d7353d424a2013c1589b7193ec8e2ef5683e2c1
Anayatc <anayatc@gmail.com>
2017-05-10 15:24:06 +0100

    flask files

---
+++ URL_Lookup.py
@@ -0,0 +1,35 @@
+import requests
+from urllib.parse import urlparse
+import whois
+
+url = 'http://amzn.to/2pfDiJE'
+print(url)
+
+
+# takes url as input and returns full url if url has been shortened.
+def url_resolve(short_url):
+    session = requests.Session()
+    resp = session.head(short_url, allow_redirects=True)
+    return resp.url
+
+
+# takes the expanded url from url_resolve and returns just the domain name.
+def domain_name():
+    final_dest = url_resolve(url)
+    parsed_uri = urlparse(final_dest)
+    domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+    return domain
+
+
+# returns the who.is information for the domain
+def who_is():
+    domain = domain_name()
+    w = whois.whois(domain)
+    print(w.text)
+    w.text.find(w.text)
+
+
+print(url_resolve(url))
+
+print(domain_name())
+print(who_is())
