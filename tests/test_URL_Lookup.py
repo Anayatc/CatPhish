37debd245eda9b37157078d1903327e6df9fce35
Anayatc <anayatc@gmail.com>
2017-05-11 11:29:21 +0100

    setting up for testing with pytest

--- tests/test_URL_Lookup.py
+++ tests/test_URL_Lookup.py
@@ -1,6 +1,6 @@
-from CyberPhish import URL_Lookup
+from URL_Lookup import *
 
 
 def test_add_scheme():
-    assert URL_Lookup.add_scheme('ow.ly/i5AX30bCN5s' == 'http://ow.ly/i5AX30bCN5s')
+    assert(add_scheme('ow.ly/i5AX30bCN5s') == 'http://ow.ly/i5AX30bCN5s')
 
