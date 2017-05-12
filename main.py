b18f09c187351ce5a021ba203c76a6f36d26c140
Anayatc <anayatc@gmail.com>
2017-05-12 14:59:45 +0100

    linking form to flask

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/send", methods=['GET','POST'])
+def send():
+    if request.method == 'POST':
+        return render_template("send.html")
 
 
 if __name__ == "__main__":
