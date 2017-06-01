995d101899ef4791f3e6da3ef4654085efb3d7b8
Anayatc <anayatc@gmail.com>
2017-06-01 13:32:48 +0100

    edited test cases

--- tests/test_URL_Lookup.py
+++ tests/test_URL_Lookup.py
@@ -2,24 +2,24 @@ from URL_Lookup import *
 
 
 def test_add_scheme():
-    assert(add_scheme('ow.ly/i5AX30bCN5s') == 'http://ow.ly/i5AX30bCN5s')
-    assert(add_scheme('google.com') == 'http://google.com')
-    assert(add_scheme('www.google.com') == 'http://www.google.com')
-    assert(add_scheme('www.amazon.com') == 'http://www.amazon.com')
-    assert(add_scheme('www.ebay.com') == 'http://www.ebay.com')
-    assert(add_scheme('www.paypal.com') == 'http://www.paypal.com')
-    assert(add_scheme('www.reddit.com') == 'http://www.reddit.com')
-    assert(add_scheme('www.apple.com') == 'http://www.apple.com')
-    assert(add_scheme('www.github.com') == 'http://www.github.com')
-    assert(add_scheme('www.quora.com') == 'http://www.quora.com')
-    assert(add_scheme('www.lloydsbank.com') == 'http://www.lloydsbank.com')
-    assert(add_scheme('www.hsbc.com') == 'http://www.hsbc.com')
-    assert(add_scheme('www.barclays.com') == 'http://www.barclays.com')
-    assert(add_scheme('apple.com') == 'http://apple.com')
+    assert(url_resolve('ow.ly/i5AX30bCN5s') == 'http://ow.ly/i5AX30bCN5s')
+    assert(url_resolve('google.com') == 'http://google.com')
+    assert(url_resolve('www.google.com') == 'http://www.google.com')
+    assert(url_resolve('www.amazon.com') == 'http://www.amazon.com')
+    assert(url_resolve('www.ebay.com') == 'http://www.ebay.com')
+    assert(url_resolve('www.paypal.com') == 'http://www.paypal.com')
+    assert(url_resolve('www.reddit.com') == 'http://www.reddit.com')
+    assert(url_resolve('www.apple.com') == 'http://www.apple.com')
+    assert(url_resolve('www.github.com') == 'http://www.github.com')
+    assert(url_resolve('www.quora.com') == 'http://www.quora.com')
+    assert(url_resolve('www.lloydsbank.com') == 'http://www.lloydsbank.com')
+    assert(url_resolve('www.hsbc.com') == 'http://www.hsbc.com')
+    assert(url_resolve('www.barclays.com') == 'http://www.barclays.com')
+    assert(url_resolve('apple.com') == 'http://apple.com')
 
 
 def test_who_is():
-    resolve = add_scheme('ow.ly/i5AX30bCN5s')
+    resolve = url_resolve('ow.ly/i5AX30bCN5s')
     assert resolve == 'https://bitbucket.org/richardpenman/pywhois/overview'
 
 
